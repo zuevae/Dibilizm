ad1731d407642ce976134e00722e2efa177e8a97
ЗуеваЕЭ <@208-11>
2023-06-05 14:56:30 +0300

    Добавьте файлы проекта.

---
+++ Dibilizm/Pages/Avtor.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dibilizm.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtor.xaml
+    /// </summary>
+    public partial class Avtor : Page
+    {
+        public Frame frame1;
+        public Avtor(Frame frame)
+        {
+            InitializeComponent();
+            frame1 = frame;
+        }
+
+        private void Entre_Click(object sender, RoutedEventArgs e)
+        {
+            var Userlog = Entities.GetContext().Users.ToList();
+            int countUser = Entities.GetContext().Users.Count();
+            for (int i = 0; i < countUser; i++)
+            {
+                if (Log.Text == Userlog[i].Login)
+                {
+                    if (Pas.Password == Userlog[i].Password)
+                    {
+                        //frame1.Navigate(new )
+                    }
+                }
+            }
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(new Registr(frame1));
+        }
+    }
+}
---
+++ Dibilizm/Pages/Registr.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dibilizm.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registr.xaml
+    /// </summary>
+    public partial class Registr : Page
+    {
+        public Frame frame1;
+        public Registr(Frame frame)
+        {
+            InitializeComponent();
+            frame1 = frame;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Navigate(new Avtor(frame1));
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            List<Users> user = new List<Users>() { new Users() };
+            string log = Log.Text;
+            string pas = Pas.Text;
+            user[0].Login = log;
+            user[0].Password = pas;
+            Entities.GetContext().Users.Add(user[0]);
+            Entities.GetContext().SaveChanges();
+            frame1.Navigate(new Avtor(frame1));
+        }
+    }
+}
